cf5260e73d5986b7c864871d5f0b733864f5da15
ukah emmanuel ogbonna <ukaemmanuel2000@gmail.com>
2023-04-26 10:20:48 +0100

    changes

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print(active_users)
--- test.py
+++ test.py
@@ -1,8 +1,9 @@
-for n in range(2, 10):
-    for x in range(2, n):
-        if n % x == 0:
-            print(n, 'equals', x, '*', n//x)
-            break
-    else:
-        # loop fell through without finding a factor
-        print(n, 'is a prime number')
+# Create a sample collection
+users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
+
+
+active_users = {}
+for k, v in users.items():
+    if v == 'active':
+        active_users[k] = v
+print(active_users)
