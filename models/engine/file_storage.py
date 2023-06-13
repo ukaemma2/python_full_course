3e60da7e0018853b26646833cccc3207aec3ab9b
ukah emmanuel ogbonna <ukaemmanuel2000@gmail.com>
2023-06-13 21:15:56 +0100

    scss

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,3 +3,13 @@ import json
 class FileStorage:
 
     __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+       return self.__objects
+
+    def new(self, obj):
+        className = obj.__class__.__name__
+        self.__objects['{}.{}'.format(className, obj.id)] = obj
+
+    def save(self):
