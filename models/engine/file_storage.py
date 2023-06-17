00ce071778426a8e6d4e1fec0f3ba9777c6fd61b
ukah emmanuel ogbonna <ukaemmanuel2000@gmail.com>
2023-06-17 13:11:29 +0100

    fileNot Found

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,4 +18,13 @@ class FileStorage:
         with open(FileStorage.__file_path, 'w') as file_handle:
             json.dump(objectDictionary, file_handle)
 
-    
+    def reload(self):
+        try:
+            with open(FileStorage.__file_path, 'w') as file_handle:
+                objectDictionary = json.load(file_handle)
+                for obj in objectDictionary.values():
+                    className = obj["__class_-"]
+                    del obj["__class__"]
+                    self.new(eval(className)(objectDictionary))
+        except FileNotFoundError:
+            return
