76658de43aa946cd29afe72b9b03ae4d11f8a565
ukah emmanuel ogbonna <ukaemmanuel2000@gmail.com>
2023-06-19 11:49:16 +0100

    changes to js

--- javascript_tutoria.js
+++ javascript_tutoria.js
@@ -1,7 +1,7 @@
 // this is my commjents
 
-
-for (let i = 1; i <= 10; i++) {
+let i;
+for (let i = 0; i <= 10; i++) {
     
     if (i % 2 == 0) {
         console.log(' this is my even number 1 to 10', i);
