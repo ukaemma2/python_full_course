e186b5fad686e3f5a625340a96d084fcef52a602
ukah emmanuel ogbonna <ukaemmanuel2000@gmail.com>
2024-02-13 16:01:56 +0100

    error

--- models/tests/test_model/test_base_model.py
+++ models/tests/test_model/test_base_model.py
@@ -33,5 +33,15 @@ class TestBaseModel(unittest.TestCase):
          self.assertEqual(my_model_dict['id'], my_model.id)
          self.assertEqual(my_model_dict['created_at'], my_model.created_at.isoformat())
          self.assertEqual(my_model_dict['updated_at'], my_model.updated_at.isoformat())
-         
+
     
+    def test_str(self):
+         my_model = BaseModel()
+
+         self.assertTrue(str(my_model).startswith('[BaseModel]'))
+         self.assertIn(my_model.id, str(my_model))
+         self.assertIn(str(my_model.__dict__), str(my_model))
+
+
+if __name__ == '__main__':
+     unittest.main()
