d6a26aad879fa45839b35770c2a63053684d83ab
ukah emmanuel ogbonna <ukaemmanuel2000@gmail.com>
2024-02-13 14:52:46 +0100

    testing

--- models/tests/test_model/test_base_model.py
+++ models/tests/test_model/test_base_model.py
@@ -17,6 +17,21 @@ class TestBaseModel(unittest.TestCase):
     def test_save(self):
          my_model = BaseModel()
          
+         initial_updated_at = my_model.updated_at
+         current_updated_at = my_model.save()
 
+         self.assertNotEqual(initial_updated_at, current_updated_at)
 
+    def test_to_dict(self):
+         my_model = BaseModel()
+
+         my_model_dict = my_model.to_dict()
+
+         self.assertIsInstance(my_model_dict, dict)
+
+         self.assertEqual(my_model_dict["__class__"], 'BaseModel')
+         self.assertEqual(my_model_dict['id'], my_model.id)
+         self.assertEqual(my_model_dict['created_at'], my_model.created_at.isoformat())
+         self.assertEqual(my_model_dict['updated_at'], my_model.updated_at.isoformat())
+         
     
