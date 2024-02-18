c890a74c86fe0e04b209aa82cf4ab35bcdc37dce
ukah emmanuel ogbonna <ukaemmanuel2000@gmail.com>
2024-02-18 16:53:44 +0100

    asserttested

--- models/tests/test_model/test_base_model.py
+++ models/tests/test_model/test_base_model.py
@@ -10,9 +10,9 @@ from models.base_model import BaseModel
 class TestBaseModel(unittest.TestCase):
     def test_init(self):
           my_model = BaseModel()
-          self.assertIsNone(my_model.id)
-          self.assertIsNone(my_model.created_at)
-          self.assertIsNone(my_model.updated_at)
+          self.assertTrue(my_model.id)
+          self.assertTrue(my_model.created_at)
+          self.assertTrue(my_model.updated_at)
 
     def test_save(self):
          my_model = BaseModel()
